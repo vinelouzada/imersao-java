5c379fd2618f7a2f96aba78aa5453c6f364493f1
Vinicius Louzada <vinelouzada@gmail.com>
2022-07-18 11:26:09 -0300

    Adicionando para o usuário dar a nota do filme

--- src/App.java
+++ src/App.java
@@ -7,6 +7,7 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -19,8 +20,9 @@ public class App {
         //fazer uma conexão HTTP
         String url = "https://imdb-api.com/en/API/Top250Movies/" + properties.getProperty("chave.imdb");
         String url2 = "https://imdb-api.com/en/API/MostPopularMovies/" + properties.getProperty("chave.imdb");
+        //temporária https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060
         
-        URI endereco = URI.create(url2);
+        URI endereco = URI.create(url);
 
         var client = HttpClient.newHttpClient();
 
@@ -58,5 +60,32 @@ public class App {
             
             System.out.println(estrelasFilme);
         }
+
+        //Pedindo avaliação
+        Scanner ler = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            Map<String,String> filme = listaDeFilmes.get(i);
+        
+            String nomeFilme = filme.get("title");
+            String imgFilme = filme.get("image");
+
+            System.out.println("------------------------");
+            System.out.println("Título: \u001b[1m" + nomeFilme + "\u001b[0m");
+            System.out.println("Poster: \u001b[1m " + imgFilme + "\u001b[0m");
+            System.out.println("Qual a sua avaliação para este filme? (0 a 10)");
+            Float notaFilme = ler.nextFloat();
+            
+            System.out.println("\u001b[33m \u001b[45m Classificação: \u001b[1m " + (notaFilme) + "\u001b[0m");
+
+            int numEstrelasFilme = Math.round(notaFilme);
+            String estrelasFilme = "";
+
+            for (int j = 0; j < numEstrelasFilme; j++) {
+                estrelasFilme += "⭐";
+            }
+            
+            System.out.println(estrelasFilme);
+        }
     }
 }
+
