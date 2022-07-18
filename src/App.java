ec7c01fdc20dd258f281ca7aa7959751d5bca9a4
Vinicius Louzada <vinelouzada@gmail.com>
2022-07-18 11:51:44 -0300

    Adicionando exceções para formato inválido

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
@@ -7,14 +8,22 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        Properties properties = new Properties();
+        FileInputStream file = new FileInputStream("../properties/conf.properties");
+        properties.load(file);
+
+
         //fazer uma conexão HTTP
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_0ysnlc3t";
-        String url2 = "https://imdb-api.com/en/API/MostPopularMovies/k_0ysnlc3t";
+        //String url = "https://imdb-api.com/en/API/Top250Movies/" + properties.getProperty("chave.imdb");
+       // String url2 = "https://imdb-api.com/en/API/MostPopularMovies/" + properties.getProperty("chave.imdb");
+        //temporária https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060
         
-        URI endereco = URI.create(url2);
+        URI endereco = URI.create("https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060");
 
         var client = HttpClient.newHttpClient();
 
@@ -54,7 +63,7 @@ public class App {
         }*/
         var geradora = new GeradoraDeFigurinhas();
         
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < listaDeFilmes.size(); i++) {
             Map<String,String> filme = listaDeFilmes.get(i);
 
             String urlImagem = filme.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
@@ -65,7 +74,11 @@ public class App {
             InputStream inputStream = new URL(urlImagem).openStream();
 
             System.out.println("Gerando imagem - [" + nomeFilme + "]");
-            geradora.cria(inputStream, nomeArquivo);
+            try{
+                geradora.cria(inputStream, nomeArquivo);
+            }catch (javax.imageio.IIOException err) {
+                System.out.println("Formato inválido de imagem");
+            }
         }
 
         System.out.println("Imagens concluídas!");
